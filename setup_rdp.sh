854c9961f8126af997d77134288d827f5d916bae
Schuelein, Lutz <lutz.schuelein@googlemail.com>
2015-12-30 20:07:14 +0100

    starting work on win-rdp setup script

---
+++ setup_rdp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#define stuff
+RDP_USER=$1
+RDP_USER_PASS=asdf
+TTY=3
+
+
+#handle user stuff
+id -u $RDP_USER >/dev/null 2>&1
+if [ $? -eq 0 ]
+	then echo "The User <$RDP_USER> allready exists.\nskipping..."
+	else 
+		echo "User: $RDP_USER does not exist, further action required"
+		adduser -m -p $RDP_USER_PASS -s /bin/bash $RDP_USER
+		if [ $? -eq 1 ]; then echo "Something went terribly wront whilst setting up the new user!"; exit 1; fi 
+		echo "User: $RDP_USER added successfully"
+fi
+
+exit 0
